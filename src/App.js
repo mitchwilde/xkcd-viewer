aff8023497073142714c9ab21a6253b31b32b465
Mitch <mitchell.wilde@gmail.com>
2022-11-12 17:25:28 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './Navbar'
+import Home from './Home'
+//import NextButton from './NextButton'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
